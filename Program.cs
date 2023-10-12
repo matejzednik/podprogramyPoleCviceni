9ce2d27896bb6be7efea7c4627530430bd74f6c5
matejzednik <matej.zednik.022@pslib.cz>
2023-10-12 09:31:12 +0200

    1. Podprogram

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Matěj Zedník P2B 12. 10. 2023
+1. Počet prvků
+2. Maximum (ne přes vestavěné funkce)
+3. Minimum
+4. Součet všech hodnot
+5. Součin všech hodnot
+6. Aritmetický průměr
+7. Geometrický průměr - pozor na to, že je definován pouze pro kladná čísla
+8. Medián (k seřazení hodnot použijte funkci Array.Sort())
+9. Četnost výskytu všech hodnot (vrátí pole, ve kterém bude počet výskytů dané hodnoty - tedy pro {1,2,1,3,5,1} vrátí {0,3,1,1,0,1} ), zde je otázka, jak se postarat o záporná čísla
+10. Pole odchylek všech prvků od dané hodnoty - tedy pro pole {4,3,2} a hodnotu 3 vrátí {1,0,-1}
+11. Rozptyl
+12. Směrodatnou odchylku*/
+
+
+//1. Podprogram
+
+static int pocetPrvku(int pocet)
+{
+    int[] pole1 = new int[] { 1, 5, 6, 8, 9 };
+    pocet = pole1.Count();
+    return pocet;
+}
+
+//2. Podprogram
+
+static int maxPocetSeznamu(int max)
+{
+    int[] pole2 = new int[] { 5, 6, 100, 200, 300, 402, 7, 8 };
+
+}
+
